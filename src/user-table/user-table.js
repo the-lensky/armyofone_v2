abf44c9ed9aaefd7541e2e45c3f020ab6f266746
ravesnature <76727818+ravesnature@users.noreply.github.com>
2021-01-09 02:17:34 +0300

    add some logics in userlist

--- src/user-table/user-table.js
+++ src/user-table/user-table.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import UserItem from './user-item'
+
+const API_URL = 'https://jsonplaceholder.typicode.com/users'
 
 const UserTable = () => {
+    const [users, getUsers] = useState([])
+    const [loadingData, setLoadingData] = useState(false)
+    const [showButton, setShowButton] = useState(true)
+
+    const getUsersData = async () => {
+        setLoadingData(true)
+        try {
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            getUsers(data)
+            setLoadingData(false)
+        } catch (e) {
+            setLoadingData(false)
+            console.log(`You've got error ${e}`)
+        }
+    }
+
+    useEffect(() => {
+        if (!showButton) {
+            getUsersData()
+        }
+    })
+
+    if (showButton) {
+        return (
+            <button
+                onClick={() => setShowButton(false)}> Push me </button>
+        )
+    }
+
+    if (loadingData) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
-            <h1>user table</h1>
+            <UserItem users={users}/>
         </div>
     )
-}
 
+}
 export default UserTable
